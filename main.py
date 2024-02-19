53105e4a99a84e12ec115a8526a08e34642c888f
NI-PA <91149653+NI-PA@users.noreply.github.com>
2024-02-19 21:55:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import pyjokes
 from selenium import webdriver
 
 
-listener = sr.Recognizer( )
+listener=sr.Recognizer( )
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice',voices[1].id)
